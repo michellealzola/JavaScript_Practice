45dd050bc8e9121527ac3fd82e7c47b79b6da7f4
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-03-05 20:15:58 -0700

    Add files via upload

---
+++ Chap01_Projects/P2/p2_project02.js
@@ -0,0 +1,9 @@
+let service1Name = 'Basic'
+let service2Name = 'Express'
+let service3Name = 'Extreme'
+let service4Name = 'Ultimate'
+let service1Speed = '10 Mbps'
+let service2Speed = '100 Mbps'
+let service3Speed = '500 Mbps'
+let service4Speed = '1 Gig'
+
