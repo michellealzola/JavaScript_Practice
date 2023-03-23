e69763b83f16915ffd2edf7a22de26e7d12862c6
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-03-23 09:52:12 -0600

    Delete Chap02_Projects directory

--- Chap02_Projects/P2/c2_p2.js
+++
@@ -1,9 +0,0 @@
-let msg = 'Please fill in all fields'
-
-document.getElementById('submitBtn').onclick = function(){
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let phone = document.getElementById('phone').value;
-
-    window.alert((name == '' | email == '' | phone == '') ? msg : 'Thank you');
-}
